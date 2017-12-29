274d209965a93a799c5075693f5816616589cfa8
wangdk789@163.com <wangdk789@163.com>
2017-12-29 16:20:14 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM registry.iop.com:5000/os/centos:7.3.1611
+MAINTAINER = DamonWang <wangdk@inspur.com>
+
+EXPOSE 5000 35357
+ENV KEYSTONE_VERSION stable/ocata
+ENV KEYSTONE_ADMIN_PASSWORD 123456a?
+ENV KEYSTONE_DB_ROOT_PASSWD 123456a?
+ENV KEYSTONE_DB_PASSWD 123456a?
+USER root
+WORKDIR /root/
+RUN rm -rf /etc/yum.repos.d/*
+COPY files/yum.repos.d /etc/yum.repos.d/
+# install keystone and mysql
+RUN yum clean all; \
+    yum makecache;\
+    yum install -y openssh-server openstack-keystone mysql dos2unix net-tools python-keystoneclient python-openstackclient  openstack-utils httpd mod_wsgi; \
+    yum clean all
+
+LABEL version="$KEYSTONE_VERSION"
+LABEL description="Openstack Keystone Docker Image Supporting HTTP/HTTPS"
+
+
+COPY files/etc/keystone.conf /etc/keystone/keystone.conf
+COPY files/keystone.sql /root/keystone.sql
+COPY files/bootstrap.sh /usr/local/bin/bootstrap.sh
+COPY files/wsgi-keystone.conf /etc/httpd/conf.d/
+RUN chmod 750 /usr/local/bin/bootstrap.sh
+
+CMD ["/usr/local/bin/bootstrap.sh"]
---
+++ files/bootstrap.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+HTTP="http"
+
+if [ -z $KEYSTONE_DB_HOST ]; then
+  echo "Please set KEYSTONE_DB_HOST when running a container."
+  exit 1;
+else
+    if [ -z $KEYSTONE_DB_ROOT_PASSWD_IF_REMOTED ]; then
+        echo "Your'are using Remote MySQL Database; "
+        echo "Please set KEYSTONE_DB_ROOT_PASSWD_IF_REMOTED when running a container."
+        exit 1;
+    else
+        KEYSTONE_DB_ROOT_PASSWD=$KEYSTONE_DB_ROOT_PASSWD_IF_REMOTED
+    fi
+fi
+
+
+# Keystone Database and user init
+if [ ! -f "/etc/keystone/initial" ]; then
+        echo "init keystone..."
+	sed -i 's|KEYSTONE_DB_PASSWD|'"$KEYSTONE_DB_PASSWD"'|g' /root/keystone.sql
+	mysql -uroot -p$KEYSTONE_DB_ROOT_PASSWD_IF_REMOTED -h $KEYSTONE_DB_HOST < /root/keystone.sql
+	# Update keystone.conf
+	sed -i "s/KEYSTONE_DB_PASSWORD/$KEYSTONE_DB_PASSWD/g" /etc/keystone/keystone.conf
+	sed -i "s/KEYSTONE_DB_HOST/$KEYSTONE_DB_HOST/g" /etc/keystone/keystone.conf
+	echo "keystone-manage db_sync...."
+        su -s /bin/sh -c 'keystone-manage db_sync' keystone
+	echo "fernet_setup..."
+        keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+        echo "credential_setup..."
+        keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+	# Populate keystone database
+        echo "bootstrap..."
+	keystone-manage bootstrap --bootstrap-password $KEYSTONE_ADMIN_PASSWORD \
+	  --bootstrap-admin-url http://$HOSTNAME:35357/v3/ \
+	  --bootstrap-internal-url http://$HOSTNAME:5000/v3/ \
+	  --bootstrap-public-url http://$HOSTNAME:5000/v3/ \
+	  --bootstrap-region-id RegionOne
+        echo "touch init file"
+        touch /etc/keystone/initial
+
+fi
+
+echo "ServerName $HOSTNAME" >> /etc/httpd/conf/httpd.conf
+echo "set openrc"
+cat > /root/openrc <<EOF
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=${KEYSTONE_ADMIN_PASSWORD}
+export OS_AUTH_URL=$HTTP://${HOSTNAME}:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+EOF
+
+/usr/sbin/httpd
+while true;do
+ sleep 60
+done
+# ensite keystone and start apache2
+# systemctl enable httpd.service
+# systemctl start httpd.service
